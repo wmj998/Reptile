97183589011d60e080aa62220c21394cf2230f7a
wmj998 <wmj998@foxmail.com>
2021-05-11 22:49:18 +0800

    Update code

--- code/爬取飘花电影网图片.py
+++ code/爬取飘花电影网图片.py
@@ -2,10 +2,10 @@ import requests
 import re
 response = requests.get('https://www.piaohua.com/html/kehuan/index.html')
 text = response.text
-urls = re.findall('<img src="(.*?)"',text)
-for i in range(1,len(urls)):
+urls = re.findall('<img src="(.*?)"', text)
+for i in range(1, len(urls)):
     img = requests.get(urls[i])
     name = urls[i].split('/')[-1]
-    f = open(name,'wb')
+    f = open(name, 'wb')
     f.write(img.content)
     f.close()
