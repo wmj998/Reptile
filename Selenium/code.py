3bb7d053c7410090a18bca16c6b60a7f9d32c28c
wmj998 <wmj998@foxmail.com>
2021-06-27 07:58:59 +0800

    Update code

---
+++ Selenium/code.py
@@ -0,0 +1,11 @@
+import time
+from selenium import webdriver
+
+browse = webdriver.Chrome()
+browse.get('https://www.baidu.com')
+input = browse.find_element_by_class_name('s_ipt')
+input.send_keys('python')
+browse.find_element_by_css_selector('.bg.s_btn').click()
+time.sleep(5)
+print(browse.page_source)
+browse.quit()
