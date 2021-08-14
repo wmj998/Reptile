bbe6e272fbfb749b8ae0c48ddf49117df037886a
wmj998 <wmj998@foxmail.com>
2021-08-14 22:18:01 +0800

    Update code.py

--- 爬取飘花电影网图片/code.py
+++ 爬取飘花电影网图片/code.py
@@ -1,12 +1,27 @@
+import threading
+import time
 import requests
 import re
 
-response = requests.get('https://www.piaohua.com/html/kehuan/list_1.html')
-text = response.text
-urls = re.findall('<img src="(.*?)"', text)
-for i in range(1, len(urls)):
-    img = requests.get(urls[i])
-    name = urls[i].split('/')[-1]
-    f = open('tmp/'+name, 'wb')
-    f.write(img.content)
-    f.close()
+
+def phspider(url_p, num):
+    for i in range(1, num):
+        url = url_p.format(i)
+        res = requests.get(url=url)
+        html = res.text
+
+        urls = re.findall('<img src="(.*?)"', html)
+        for j in range(1, len(urls)):
+            img = requests.get(url=urls[j])
+            name = urls[j].split('/')[-1]
+            f = open('tmp/'+name, 'wb')
+            f.write(img.content)
+            f.close()
+
+
+url_p = 'https://www.piaohua.com/html/kehuan/list_{}.html'
+num = 105
+
+thread = threading.Thread(target=phspider, args=(url_p, num))
+thread.start()
+
